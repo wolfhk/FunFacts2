7252aac3890468ed9c47d6cfa94139e40c73c206
Wolfgang Ettlich <Wolfgang.Ettlich@gmx.net>
2016-03-13 23:01:25 +0800

    Initial Commit

---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Wolfgang Ettlich on 13/3/2016.
+//  Copyright © 2016 Wolfgang Ettlich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
