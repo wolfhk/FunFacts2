ff00d60020b576da8cb1bf7f2da963fc2b8c6fe3
Wolfgang Ettlich <Wolfgang.Ettlich@gmx.net>
2016-03-21 23:50:58 +0800

    uilabel

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var funFactLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        funFactLabel.text = "An interesting fact!"
     }
 
     override func didReceiveMemoryWarning() {
